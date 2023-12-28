fa4a6d62d23a7478d051b35236acea70385e4489
Mambaul Hisam <hisyamsquartz@gmail.com>
2023-12-28 20:30:30 +0700

    sort article  by date

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,4 +1,5 @@
-import { open, readdir, mkdir } from "fs/promises";
+import { open, readdir, mkdir, stat } from "fs/promises";
+import { join } from "path";
 
 export namespace fs {
     export function write(path: string, data: string): void {
@@ -21,14 +22,18 @@ export namespace fs {
     }
 
     export async function readDir(dir: string, fileFormat: string): Promise<string[]> {
-        const files = await readdir(dir)
-        let htmlFiles = []
-        for (const file of files) {
-            if (file.endsWith(fileFormat)) {
-                htmlFiles.push(file)
-            }
-        }
-        return htmlFiles;
+        const order = 1
+        const files = await readdir(dir);
+        const stats = await Promise.all(
+        files.map((filename) =>
+            stat(join(dir, filename))
+                .then((stat) => ({ filename, mtime: stat.mtime }))
+            )
+        );
+
+        return stats.sort((a, b) =>
+            order * (b.mtime.getTime() - a.mtime.getTime())
+        ).map((stat) => stat.filename);
     }
 
     export function createDir(path: string, callback?: (newPath: string | undefined | null, err: any) => void): void {
